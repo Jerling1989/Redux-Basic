25cf26a7514246aa07570c9dac4648275a6942e5
Jacob <jerling1989@gmail.com>
2018-05-22 17:29:36 -0400

    conditional rendering

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -5,8 +5,17 @@ import { connect } from 'react-redux';
 // CREATE BOOKDETAIL CLASS COMPONENT
 class BookDetail extends Component {
 	render() {
+		// IF THIS.PROPS.BOOK DOESN'T EXIST YET RETURN EARLY
+		if (!this.props.book) {
+			return <h3>Select a book</h3>;
+		}
+		// RETURN BOOK TITLE INFO
 		return (
-			<div>Book Detail</div>
+			<div>
+				<h3>Details For:</h3>
+				<h5>Title: {this.props.book.title}</h5>
+				<h6>Pages: {this.props.book.pages}</h6>
+			</div>
 		);
 	}
 }
@@ -19,4 +28,4 @@ function mapStateToProps(state) {
 }
 
 // CREATE CONTAINER BY CONNECTING COMPONENT TO REDUX REDUCERS
-export default connect(mapStateToProps)(BookDetailo);
+export default connect(mapStateToProps)(BookDetail);
